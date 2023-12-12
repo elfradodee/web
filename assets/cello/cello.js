e95ee7d58e9c0f4dc089a355bd6df82f69ec40ba
elfradodee <87368106+elfradodee@users.noreply.github.com>
2023-12-12 14:10:26 +0700

    Added Multi-Language & Skills

--- assets/cello/cello.js
+++ assets/cello/cello.js
@@ -2,34 +2,38 @@
 
 setTimeout(function() {
     document.getElementById('preloader').classList.add('hide');
-}, 2000);
+}, 0);
 
 // MENU
 
 var divAbout    = document.getElementById('about');
 var divEdu      = document.getElementById('edu');
-var divSkill    = document.getElementById('skill');
+var divExp      = document.getElementById('exp');
 var divAward    = document.getElementById('award');
+var divSkill    = document.getElementById('skill');
 var divContact  = document.getElementById('contact');
 
 var btnAbout    = document.getElementById('btnAbout');
 var btnEdu      = document.getElementById('btnEdu');
-var btnSkill    = document.getElementById('btnSkill');
+var btnExp      = document.getElementById('btnExp');
 var btnAward    = document.getElementById('btnAward');
+var btnSkill    = document.getElementById('btnSkill');
 var btnContact  = document.getElementById('btnContact');
 
 function menuAbout() {
 
     divAbout.classList.remove('d-none');
     divEdu.classList.add('d-none');
-    divSkill.classList.add('d-none');
+    divExp.classList.add('d-none');
     divAward.classList.add('d-none');
+    divSkill.classList.add('d-none');
     divContact.classList.add('d-none');
 
     btnAbout.classList.add('active');
     btnEdu.classList.remove('active');
-    btnSkill.classList.remove('active');
+    btnExp.classList.remove('active');
     btnAward.classList.remove('active');
+    btnSkill.classList.remove('active');
     btnContact.classList.remove('active');
 
 }
@@ -38,30 +42,34 @@ function menuEdu() {
 
     divAbout.classList.add('d-none');
     divEdu.classList.remove('d-none');
-    divSkill.classList.add('d-none');
+    divExp.classList.add('d-none');
     divAward.classList.add('d-none');
+    divSkill.classList.add('d-none');
     divContact.classList.add('d-none');
 
     btnAbout.classList.remove('active');
     btnEdu.classList.add('active');
-    btnSkill.classList.remove('active');
+    btnExp.classList.remove('active');
     btnAward.classList.remove('active');
+    btnSkill.classList.remove('active');
     btnContact.classList.remove('active');
 
 }
 
-function menuSkill() {
+function menuExp() {
 
     divAbout.classList.add('d-none');
     divEdu.classList.add('d-none');
-    divSkill.classList.remove('d-none');
+    divExp.classList.remove('d-none');
     divAward.classList.add('d-none');
+    divSkill.classList.add('d-none');
     divContact.classList.add('d-none');
 
     btnAbout.classList.remove('active');
     btnEdu.classList.remove('active');
-    btnSkill.classList.add('active');
+    btnExp.classList.add('active');
     btnAward.classList.remove('active');
+    btnSkill.classList.remove('active');
     btnContact.classList.remove('active');
 
 }
@@ -70,14 +78,34 @@ function menuAward() {
 
     divAbout.classList.add('d-none');
     divEdu.classList.add('d-none');
-    divSkill.classList.add('d-none');
+    divExp.classList.add('d-none');
     divAward.classList.remove('d-none');
+    divSkill.classList.add('d-none');
     divContact.classList.add('d-none');
 
     btnAbout.classList.remove('active');
     btnEdu.classList.remove('active');
-    btnSkill.classList.remove('active');
+    btnExp.classList.remove('active');
     btnAward.classList.add('active');
+    btnSkill.classList.remove('active');
+    btnContact.classList.remove('active');
+
+}
+
+function menuSkill() {
+
+    divAbout.classList.add('d-none');
+    divEdu.classList.add('d-none');
+    divExp.classList.add('d-none');
+    divAward.classList.add('d-none');
+    divSkill.classList.remove('d-none');
+    divContact.classList.add('d-none');
+
+    btnAbout.classList.remove('active');
+    btnEdu.classList.remove('active');
+    btnExp.classList.remove('active');
+    btnAward.classList.remove('active');
+    btnSkill.classList.add('active');
     btnContact.classList.remove('active');
 
 }
@@ -86,14 +114,16 @@ function menuContact() {
 
     divAbout.classList.add('d-none');
     divEdu.classList.add('d-none');
-    divSkill.classList.add('d-none');
+    divExp.classList.add('d-none');
     divAward.classList.add('d-none');
+    divSkill.classList.add('d-none');
     divContact.classList.remove('d-none');
 
     btnAbout.classList.remove('active');
     btnEdu.classList.remove('active');
-    btnSkill.classList.remove('active');
+    btnExp.classList.remove('active');
     btnAward.classList.remove('active');
+    btnSkill.classList.remove('active');
     btnContact.classList.add('active');
 
 }
